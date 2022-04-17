17f5871629999fc09328df4706817c1949a763ba
justinkloppenborg <77425202+justinkloppenborg@users.noreply.github.com>
2022-04-17 15:42:57 -0500

    Update APIService.js

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,49 +1,49 @@
- /* eslint-disable */
+  /* eslint-disable */
 import axios from 'axios';
 //const API_URL = 'http://justinkloppenborg.pythonanywhere.com/';
-const API_URL = 'http://justinkloppenborg.pythonanywhere.com/';
+const API_URL = 'https://justinkloppenborg.pythonanywhere.com/';
 
 export class APIService {
   constructor() {
 
   }
 
-   getCustomer(param_pk) {
-     const url = `${API_URL}/api/customers/${param_pk}`;
-     let jwtToken = localStorage.getItem('token');
-     console.log(":::jwtToken:::::"+jwtToken);
-     const headers = {Authorization: `jwt ${jwtToken}`};
-     return axios.get(url, {headers: {Authorization: `jwt ${jwtToken}`}});
+  getCustomer(param_pk) {
+    const url = `${API_URL}/api/customers/${param_pk}`;
+    let jwtToken = localStorage.getItem('token');
+    console.log(":::jwtToken:::::" + jwtToken);
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.get(url, { headers: { Authorization: `jwt ${jwtToken}` } });
   }
 
-   getCustomerList() {
+  getCustomerList() {
     const url = `${API_URL}/api/customers/`;
     let jwtToken = localStorage.getItem('token');
-    console.log(":::jwtToken:::::"+jwtToken);
-    const headers = {Authorization: `jwt ${jwtToken}`};
-    return axios.get(url, {headers: headers});
+    console.log(":::jwtToken:::::" + jwtToken);
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.get(url, { headers: headers });
 
   }
 
-  addNewCustomer(customer){
-   const url = `${API_URL}/api/customers/`;
-   let jwtToken = localStorage.getItem('token');
-   const headers = {Authorization: `jwt ${jwtToken}`};
-   return axios.post(url, customer, {headers: headers});
+  addNewCustomer(customer) {
+    const url = `${API_URL}/api/customers/`;
+    let jwtToken = localStorage.getItem('token');
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.post(url, customer, { headers: headers });
   }
 
-  updateCustomer(customer){
+  updateCustomer(customer) {
     const url = `${API_URL}/api/customers/${customer.pk}`;
     let jwtToken = localStorage.getItem('token');
-    const headers = {Authorization: `jwt ${jwtToken}`};
-    return axios.put(url, customer, {headers: headers});
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.put(url, customer, { headers: headers });
   }
 
-  deleteCustomer(customer_Pk){
-     const url = `${API_URL}/api/customers/${customer_Pk}`;
-     let jwtToken = localStorage.getItem('token');
-     const headers = {Authorization: `jwt ${jwtToken}`};
-     return axios.delete(url, {headers: headers});
+  deleteCustomer(customer_Pk) {
+    const url = `${API_URL}/api/customers/${customer_Pk}`;
+    let jwtToken = localStorage.getItem('token');
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.delete(url, { headers: headers });
   }
 
   authenticateLogin(credentials) {
@@ -51,10 +51,108 @@ export class APIService {
     return axios.post(url, credentials);
   }
 
- registerUser(credentials) {
+  registerUser(credentials) {
     const url = `${API_URL}/register/`;
     credentials.customusername = credentials.username
     return axios.post(url, credentials);
   }
+
+  getInvestment(param_pk) {
+    const url = `${API_URL}/api/investments/${param_pk}`;
+    let jwtToken = localStorage.getItem('token');
+    console.log(":::jwtToken:::::" + jwtToken);
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.get(url, { headers: { Authorization: `jwt ${jwtToken}` } });
+  }
+
+
+
+  getInvestmentList() {
+    const url = `${API_URL}/api/investments`;
+    let jwtToken = localStorage.getItem('token');
+    console.log(":::jwtToken:::::" + jwtToken);
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.get(url, { headers: headers });
+
+
+
+  }
+
+
+
+  addNewInvestment(investment) {
+    const url = `${API_URL}/api/investments/`;
+    let jwtToken = localStorage.getItem('token');
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.post(url, investment, { headers: headers });
+  }
+
+
+
+  updateInvestment(investment) {
+    const url = `${API_URL}/api/investments/${investment.pk}`;
+    let jwtToken = localStorage.getItem('token');
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.put(url, investment, { headers: headers });
+  }
+
+
+
+  deleteInvestment(investment_Pk) {
+    const url = `${API_URL}/api/investments/${investment_Pk}`;
+    let jwtToken = localStorage.getItem('token');
+    const headers = { Authorization: `jwt ${jwtToken}` };
+    return axios.delete(url, { headers: headers });
+  }
+
+
+  
+   getStock(param_pk) {
+   const url = `${API_URL}/api/stocks/${param_pk}`;
+   let jwtToken = localStorage.getItem('token');
+   console.log(":::jwtToken:::::"+jwtToken);
+   const headers = {Authorization: `jwt ${jwtToken}`};
+   return axios.get(url, {headers: {Authorization: `jwt ${jwtToken}`}});
+}
+
+
+
+ getStockList() {
+     const url = `${API_URL}/api/stocks`;
+     let jwtToken = localStorage.getItem('token');
+     console.log(":::jwtToken:::::" + jwtToken);
+     const headers = {Authorization: `jwt ${jwtToken}`};
+     return axios.get(url, {headers: headers});
+
+
+
+ }
+
+
+
+  addNewStock(stock){
+ const url = `${API_URL}/api/stocks/`;
+ let jwtToken = localStorage.getItem('token');
+ const headers = {Authorization: `jwt ${jwtToken}`};
+ return axios.post(url, investment, {headers: headers});
+}
+
+
+
+updateStock(stock){
+  const url = `${API_URL}/api/stocks/${investment.pk}`;
+  let jwtToken = localStorage.getItem('token');
+  const headers = {Authorization: `jwt ${jwtToken}`};
+  return axios.put(url, investment, {headers: headers});
+}
+
+
+
+deleteStock(stock_Pk){
+   const url = `${API_URL}/api/stocks/${investment_Pk}`;
+   let jwtToken = localStorage.getItem('token');
+   const headers = {Authorization: `jwt ${jwtToken}`};
+   return axios.delete(url, {headers: headers});
+}
 
 }
